018730048a6cb7ab5db8e0f594ca3a6a89cfdd7c
linu1983 <linu1983@gmail.com>
2021-04-09 22:37:01 +0900

    Update ui.cc

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -375,13 +375,13 @@ static void update_status(UIState *s) {
 
 static void update_extras(UIState *s)
 {
-   if(s->awake && s->status != STATUS_OFFROAD)
-   {
+  if(s->awake && s->status != STATUS_OFFROAD)
+  {
         int touch_x = -1, touch_y = -1;
         int touched = touch_poll(&(s->touch), &touch_x, &touch_y, 0);
         dashcam(s, touch_x, touch_y);
         touched = touched + 0;
-   }
+  }
 }
 
 void ui_update(UIState *s) {
